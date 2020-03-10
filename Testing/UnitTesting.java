a57860db3f1712dbf1440c6794a5d19838a25777
Kyler Briscoe <Kyler.Briscoe@gmail.com>
2020-03-09 20:06:38 -0400

    Added address input test

--- Testing/UnitTesting.java
+++ Testing/UnitTesting.java
@@ -2,12 +2,22 @@ package Testing;
 
 //Testing imports
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+//Modules
 import AddressBook.AddressBook;
 import AddressBook.AddressBookController;
 import AddressBook.FileSystem;
 import AddressBook.Person;
+
+//Imports
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class UnitTesting {
 
@@ -42,5 +52,12 @@ public class UnitTesting {
                 "City Name", "State Name", "12345", ""));
 
 */
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"12345 House Rd", "9594 Community Lane", "error"})
+    void testPersonInput(String input) {
+        String addressPattern = "(\\d{1,5}\\s\\w+\\s\\w+)";
+        assertTrue(input.matches(addressPattern));
     }
 }
